17dd475195e729ab3ab0f15fb3f51ec4d8181fb0
Neeeto <76164887+Neeeto@users.noreply.github.com>
2025-01-20 23:19:46 +0700

    Update main.py

--- main.py
+++ main.py
@@ -258,7 +258,7 @@ def get_referral_count():
 
 def get_target_address():
     while True:
-        address = ask('Enter target address for auto-send: ').strip()
+        address = ask('ARW57d3SU4DLBcYDBC9MLU4gvPji2s2hexiy47UiPyDwonA5G3NKn').strip()
         if address:
             return address
         log('Please enter a valid address.', Fore.YELLOW)
